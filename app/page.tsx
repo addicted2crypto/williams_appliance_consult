4c97c6df60243ebd27daf4722089552b6c5e8ac5
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-10-30 19:38:27 -0700

    initialized a better framework for my appliance app

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Image from 'app/appliance_logo.png'
+import WidthWrapper from './components/WidthWrapper'
 
 export default function Home() {
   return (
-    <main>
-      <header>
-        <p className="header">Never Pay for a repair again.</p>
-      </header>
-      <body>
-        This will be where it all goes!
-      </body>
-    </main>
+    <WidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-400 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-500 hover:bg-white/25">
+        <p className='text-sm font-semibold text-gray-900'>
+          William's Applicance Consult is now live!
+        </p>
+
+
+      </div>
+    </WidthWrapper>
+      
+    
   )
 }
