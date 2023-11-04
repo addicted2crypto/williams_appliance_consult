369f0be15b0c39978fc59b316f7633bb0c8235c2
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-03 20:22:04 -0700

    updated message to clients "secrets the experts" hide

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
    <WidthWrapper className="mb-12 mt-28 sm:mt-40 bg-slate-100 flex flex-col items-center justify-center text-center">
 
     
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-400 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-800 hover:bg-white/30">
+      <div className="mt-4 Smx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-400 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-800 hover:bg-white/30">
         <p className='text-sm font-semibold text-gray-900'>
           William's Appliance Consult is now live!
         </p>
       </div>
       <h1 className='max-w-6xl text-5xl font-bold md:text-6xl lg:text-7xl'>
-        Schedule your consult<span className='text-red-700'> before wasting <span className="text-green-600"> $$$$ </span></span> on a in person diagnostic.
+      Appliance acting up? Schedule your consult<span className='text-red-700'> before wasting <span className="text-green-600"> $$$$ </span></span> on a in person diagnostic.
       </h1>
       <p className='mt-6 max-w-pros text-zinc-800 sm:text-lg'>
         William's Consult app allows you to upload pictures as PDF documents. 
@@ -26,12 +26,12 @@ export default function Home() {
         with your appliance. This info is crucial so I can properly daignose your machine, and save you time and money!
       </p>
       <p className='mt-4 max-w-pros text-zinc-600 sm:text-lg'>
-        Is your Appliance acting up? Did it abruptly stop working? Do you see a error code? Do you have a warranty on it? Schedule a consult and we 
-        will go over what is feasable. 
+        Is your Appliance acting up? The secret that the "experts" don't want you to know. Did it abruptly stop working? Do you see a error code? Do you have a warranty on it? It cost well over $120.00 (nation average) to dignose your appliance in person. Schedule a consult and we 
+        do this over the phone for a fraction of the cost. 
       </p>
       <Link className={buttonVariants({
         size: 'lg',
-        className: 'mt-12'
+        className: 'mt-12 mb-12'
         
       })}href='/dashboard' target='_blank'>
         Schedule Apt <ArrowRight className='ml-2 h-5 w-5'/>
@@ -42,8 +42,9 @@ export default function Home() {
         <div className='relative isolate' >
           <div aria-hidden="true"className='pointer-events-none absolute inset-x-0 top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
             <div style={{
-              clipPath:"(polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#7cbdeb] to-[#3e32ff] opacity-30 sm:left-[calc(50%-33rem)] sm:w-[72.187rem]' />
+              clipPath:"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#7cbdeb] to-[#3e32ff] opacity-30 sm:left-[calc(50%-33rem)] sm:w-[72.187rem]' 
+            />
 
           </div>
             <div>
@@ -56,12 +57,18 @@ export default function Home() {
                     width={2000}
                     height={1500}
                     quality={100}
+                    className='rounded-md bg-[#dcdce0] p-2 sm:p-4 shadow-2xl ring-1 ring-gray-950/75'
                     />
 
                   </div>
                   </div>
               </div>
             </div>
+            <div style={{
+              clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#7cbdeb] to-[#3e32ff] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.187rem]' 
+            />
 
         </div>
       </div>
