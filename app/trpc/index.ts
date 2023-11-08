cbec54b2624b98d82871d5baf67388b566fe4b59
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-07 20:44:15 -0800

    prisma added

--- app/trpc/index.ts
+++ app/trpc/index.ts
@@ -1,9 +1,18 @@
-import { router } from './trpc';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/server';
+import { TRPCError } from '@trpc/server';
+import { publicProcedure, router } from './trpc';
 
 
-const appRouter = router({
+export const appRouter = router({
+   authCallback: publicProcedure.query(() =>{
+    const {getUser} = getKindeServerSession()
+    const user = getUser()
+    if(!user.id || !user.email)
+    throw new TRPCError({ code: 'UNAUTHORIZED'})
+    return { success: true}
+   })
+    })
 
-});
 
 
 export type AppRouter = typeof appRouter;
