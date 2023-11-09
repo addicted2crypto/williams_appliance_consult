40983d900c0cf178827419d2cb20785cfe19811a
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-08 16:54:41 -0800

    async for await getUser in approuter

--- app/trpc/index.ts
+++ app/trpc/index.ts
@@ -1,17 +1,21 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { TRPCError } from '@trpc/server';
+
 import { publicProcedure, router } from './trpc';
 
 
 
 export const appRouter = router({
-   authCallback: publicProcedure.query(() =>{
+   authCallback: publicProcedure.query (async () =>{
     const { getUser } = getKindeServerSession()
-    const User = getUser()
+    const User =  await getUser()
+
     if(!User.id || !User.email) 
     throw new TRPCError({ code: 'UNAUTHORIZED'})
+
+
     return { success: true}
-   })
+   }),
     })
 
 
