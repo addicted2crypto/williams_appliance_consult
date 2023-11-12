9fe2c29c92b477ff9535126765a441f78c912b72
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-11 21:16:57 -0800

    moved _trpc to root

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -2,7 +2,7 @@
 
 import { Loader2 } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
-import { trpc } from "../_trpc/client"
+import { trpc } from "../../_trpc/client"
 
 
 const Page = () => {
@@ -27,7 +27,7 @@ trpc.authCallback.useQuery(undefined, {
     retry: true,
     retryDelay: 500,
   })
-  
+
  return (
     <div className="w-full mt-24 flex justify-center">
         <div className="flex flex-col items-center gap-2">
