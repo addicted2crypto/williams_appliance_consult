4320c35cbb3289a3038fa2e07c0097ddc127e790
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-13 16:44:58 -0800

    changed finally message

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,5 +1,5 @@
 const Dashboard = () => {
-    return (<div>  Its actually working after a weekend of TRPC kicking my ass!</div>)
+    return (<div> Pay scale and db for customer information incoming!</div>)
 }
 
 export default Dashboard
