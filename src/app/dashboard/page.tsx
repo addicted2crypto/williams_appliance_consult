4c7b1a853f3c08cb171630a215c8685e01937e44
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-21 14:11:59 -0800

    console log to find errors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,12 +2,14 @@
 import Dashboard from '@/src/components/Dashboard'
 import { db } from '@/src/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
 import { redirect } from 'next/navigation'
 
 const Page = async () => {
   const {getUser}  = getKindeServerSession();
-  const user =  await getUser()
- 
+  const user = await getUser()
+  
+ {user? console.log(user.id) : console.log('no user')}
 
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 const dbUser = await db.user.findFirst({
