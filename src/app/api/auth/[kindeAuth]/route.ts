0465b0ef23d01d7de14be4243a1957d2cee6d84f
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-22 21:13:45 -0800

    back to original-> now to find the failure for compiling

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,24 +1,24 @@
 
-// import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-// import { NextRequest } from 'next/server'
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
 
-// export async function GET(
-//   request: NextRequest,
-//   { params }: any
-// ) {
-//   const endpoint = params.kindeAuth
-//   return handleAuth(request, endpoint)
-// }
+export async function GET(
+  request: NextRequest,
+  { params }: any
+) {
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
+}
 // src/app/api/auth/[kindeAuth]/route.ts
-import { handleAuth, AuthResult } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextRequest, NextResponse } from 'next/server';
+// import { handleAuth, AuthResult } from '@kinde-oss/kinde-auth-nextjs/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-const handler = async (request: NextRequest, { params }: any): Promise<void | NextResponse> => {
-  const endpoint = params.kindeAuth;
-  const authResult: AuthResult = await handleAuth(request, endpoint);
-  return authResult.response;
-};
+// const handler = async (request: NextRequest, { params }: any): Promise<void | NextResponse> => {
+//   const endpoint = params.kindeAuth;
+//   const authResult: AuthResult = await handleAuth(request, endpoint);
+//   return authResult.response;
+// };
 
-export default handler;
+// export default handler;
 
 
