1e454f36e429f33d1ae7d7c44321db1b18741e7a
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-22 21:02:14 -0800

    trying a new auth handler

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,11 +1,22 @@
 
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest } from 'next/server'
+// import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+// import { NextRequest } from 'next/server'
 
-export async function GET(
+// export async function GET(
+//   request: NextRequest,
+//   { params }: any
+// ) {
+//   const endpoint = params.kindeAuth
+//   return handleAuth(request, endpoint)
+// }
+
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest, NextResponse } from 'next/server'
+
+export default async function handler(
   request: NextRequest,
   { params }: any
-) {
+): Promise<void | NextResponse> {
   const endpoint = params.kindeAuth
   return handleAuth(request, endpoint)
 }
