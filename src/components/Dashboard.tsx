e2a68f9d73d175756dea1e7d360211c8ee055ca7
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-23 20:52:09 -0800

    trpc middleware/skeleton/userfile schema added

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,13 @@
-import { userAgent } from "next/server"
+"use client"
+
+import { trpc } from "@/_trpc/client"
+import { Ghost } from "lucide-react"
 import UploadFileButton from "./UploadFileButton"
+import Skeleton from "react-loading-skeleton"
 
 const Dashboard = () => {
+    const {data: appliances, isLoading} = trpc.getUserFiles.useQuery()
+
     return (
     <main className="mx-auto max-w-7xl md:p-10">
         <div className="mt-8 flex flex-col justify-between gap-4 border-b border=gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
@@ -11,10 +17,23 @@ const Dashboard = () => {
             </h1>
             <UploadFileButton />
         </div>
+
         {/* display appliance info on dashboad for the logged in user */}
-        
+
+        {appliances && appliances?.length !== 0 ? (
+            <div></div>
+        ): isLoading ? (
+            <div></div>
+        ) : (
+         <div className="mt-16 flex flex-col items-center gap-2">
+            <Ghost className="h-8 w-8 text-zinc-800"/>
+            <h3 className="font-semibold tx-xl">No appliances yet</h3>
+            <p>Lets&apos;s upload your appliance</p>
+         </div>
+        )}
 
     </main>
+    
         
     )
     
