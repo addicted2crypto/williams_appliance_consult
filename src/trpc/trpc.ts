298dc1e05d63066776ee7585ad80461f509020ef
William(addicted2crypto) <addicted2krypto@gmail.com>
2023-11-26 10:26:31 -0800

    components for kinde import? module not found with server

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,4 +1,4 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/components';
 import { initTRPC, TRPCError } from '@trpc/server'
 
 const t = initTRPC.create();
