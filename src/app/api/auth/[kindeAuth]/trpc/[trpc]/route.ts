ae4991f5d790d05ba8d77cd08122e1b20bf212fa
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-08-25 01:54:08 -0700

    api cleanup

--- src/app/api/auth/[kindeAuth]/trpc/[trpc]/route.ts
+++ src/app/api/auth/[kindeAuth]/trpc/[trpc]/route.ts
@@ -1,39 +1,13 @@
-// import { appRouter } from '@/src/trpc';
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
-// import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { appRouter } from '@/src/trpc'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 
-// interface KindeAuthContext {
-//   kindeAuth: ReturnType<typeof getKindeServerSession>;
-// }
-// // const createContext = async ({req, res}: trpcNext.CreateNextContextOptions) => {
-// //   const kindeAuth = await getKindeServerSession(req, res);
-// //   return {
-// //       kindeAuth
-// //   };
-// // };
-// const {getUser} = getKindeServerSession();
-// const user = await getUser();
 
-// console.log(user || `${getUser}`);
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req,
+    router: appRouter,
+    createContext: () => ({}),
+  })
 
-// const handler = (req: Request) =>
-//   fetchRequestHandler({
-//     endpoint: '/api/trpc',
-//     req,
-//     router: appRouter,
-//     createContext: () => ({}),
-//   })
-
-// export { handler as GET, handler as POST };
-
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
-import { NextRequest } from 'next/server';
-
-export async function Get(
-  request: NextRequest,
-  { params }: any
-) {
-  const endpoint = params.kindeAuth
-  return handleAuth(request, endpoint)
-}
-
+export { handler as GET, handler as POST }
